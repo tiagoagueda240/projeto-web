787a7fff85807daf1179cd2dbfb4e150365019e1
tiagoagueda240 <geral@tiagoagueda.pt>
2022-05-28 16:03:47 +0100

    first commit

--- portfolio/models.py
+++ portfolio/models.py
@@ -24,7 +24,7 @@ class Post(models.Model):
     autor = models.ForeignKey(Pessoa, on_delete = models.CASCADE, related_name = "autor")
     data = models.DateTimeField(auto_now_add=True)
     descricao = models.CharField(max_length=1000)
-    link = models.CharField(max_length=1000)
+    link = models.CharField(max_length=1000, blank=True)
     imagem = models.ImageField()
 
     def __str__(self):
