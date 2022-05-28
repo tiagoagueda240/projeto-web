7f86d7083988ddad310f4b56d2f0c579a9d0a20a
tiagoagueda240 <geral@tiagoagueda.pt>
2022-05-28 18:04:28 +0100

    erro mat

--- portfolio/views.py
+++ portfolio/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
-import matplotlib 
 from matplotlib import pyplot as plt
 
 from .models import *
